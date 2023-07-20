f996c33194aa0d51a9b56f2514b586ac5e746f41
joaogdantas <joaogdantasdev@hotmail.com>
2023-07-20 13:56:46 -0300

    Add basic project structure

---
+++ src/main/java/com/joaogdantas/QuizQuizApp/QuizQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.joaogdantas.QuizQuizApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizQuizAppApplication.class, args);
+	}
+
+}
